cde776ece176ed805d013ff78cd61fc634a20ae6
li-zhiyang <944794859@qq.com>
2018-08-19 17:16:43 +0800

    重构

--- src/main/java/cn/hfutlzy/vuejs/controller/ViewController.java
+++ src/main/java/cn/hfutlzy/vuejs/controller/ViewController.java
@@ -1,19 +1,19 @@
 package cn.hfutlzy.vuejs.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : lzyli
  * CreateDate : 2018/8/19 16:42
  * Description :
  */
-@RestController
+@Controller
 public class ViewController {
 
     @RequestMapping("/")
-    public String getIndex(){
-        return "index";
+    public String getIndex() {
+        return "/index.html";
     }
 
 }
