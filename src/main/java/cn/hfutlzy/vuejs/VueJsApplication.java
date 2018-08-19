3f4446e231af4eb055e9c740d415f7278787a30a
li-zhiyang <944794859@qq.com>
2018-08-19 16:38:44 +0800

    重构

---
+++ src/main/java/cn/hfutlzy/vuejs/VueJsApplication.java
@@ -0,0 +1,12 @@
+package cn.hfutlzy.vuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueJsApplication.class, args);
+	}
+}
--- src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-4536573333
-</body>
-</html>
